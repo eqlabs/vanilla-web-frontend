7cf4c732a6b37abe79ddb67b7c98984961d3da9d
Lauri Junkkari <latenssi@gmail.com>
2018-02-09 14:03:15 +0200

    Use backend for proxywallet creation

--- src/controllers/Orders/proxyWallet.js
+++ src/controllers/Orders/proxyWallet.js
@@ -1,22 +1,9 @@
-import { generateETHAddress } from "../../util/eth";
 import { setOrderStatus } from "./orders";
 
-const proxyWalletsKey = "proxyWallets";
-
 export async function getProxyWalletForOrder(orderId) {
-  const proxyWallets = JSON.parse(
-    localStorage.getItem(proxyWalletsKey) || "[]"
-  );
-  let proxyWallet = proxyWallets.find(pw => pw.orderId === orderId);
-  if (!proxyWallet) {
-    proxyWallet = {
-      orderId,
-      address: generateETHAddress()
-    };
-    proxyWallets.push(proxyWallet);
-    await new Promise(resolve => setTimeout(resolve, 5000));
-  }
-  localStorage.setItem(proxyWalletsKey, JSON.stringify(proxyWallets));
+  const address = (await fetch(
+    "http://localhost:8080/proxywallet/create"
+  )).json();
   setOrderStatus(orderId, 3);
-  return proxyWallet.address;
+  return address;
 }
